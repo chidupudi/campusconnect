d7ce619a2ac8a06a849e5e7500a4e3742fc51d3f
Druthi07 <alluridruthisaisrivarma>
2025-09-18 05:40:12 +0530

    Fix test dependencies and CI/CD pipeline

--- cc/src/App.test.js
+++ cc/src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Campus Connect', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Campus Connect/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('renders Login button', () => {
+  render(<App />);
+  const loginButton = screen.getByText(/Login/i);
+  expect(loginButton).toBeInTheDocument();
 });
